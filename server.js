33e2ce558afa27b0205349d7e68d7a8581d2a434
liogiladi <98516489+liogiladi@users.noreply.github.com>
2024-06-01 12:15:42 +0300

    init server + npm

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import { createServer } from "node:http";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { Server } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+const port = process.env.PORT || 8080;
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get("/", (_req, res) => {
+    res.sendFile(join(__dirname, "client", "index.html"));
+});
+
+app.get("/test", (_req, res) => {
+    res.send("yes!");
+});
+
+io.on("connection", (socket) => {
+    console.log("a user connected to " + socket.id);
+
+    socket.on("input change", (value) => {
+        socket.broadcast.emit("input change", value);
+    });
+});
+
+server.listen(port, () => {
+    console.log("server running at port " + port);
+});
